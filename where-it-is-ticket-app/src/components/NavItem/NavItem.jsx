94f2a41f42fe3ea7b712ed8de5da0518cf3c3249
lammai442 <lam.mai442@gmail.com>
2025-04-25 15:22:45 +0200

    Starting on setting navigation

--- where-it-is-ticket-app/src/components/NavItem/NavItem.jsx
+++ where-it-is-ticket-app/src/components/NavItem/NavItem.jsx
@@ -1,8 +1,20 @@
 import './navItem.css';
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 function NavItem({ text, Icon }) {
+	const [isActive, setIsActive] = useState('false');
+	const { url } = useParams();
+
+	useEffect(() => {
+		setIsActive(url);
+		console.log(isActive);
+	}, [url]);
+
 	return (
 		<li className='nav__list-item'>
-			{Icon && <Icon className='nav__icon' />}
+			<figure className='nav__icon-box'>
+				{Icon && <Icon className='nav__icon' />}
+			</figure>
 			<p className='nav__title'>{text}</p>
 		</li>
 	);
