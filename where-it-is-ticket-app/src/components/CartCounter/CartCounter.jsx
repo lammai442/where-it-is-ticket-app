b0066b9e89b9522d828633ee790a8fa790a18f9d
lammai442 <lam.mai442@gmail.com>
2025-04-29 11:48:32 +0200

    Finished with cartCount

--- where-it-is-ticket-app/src/components/CartCounter/CartCounter.jsx
+++ where-it-is-ticket-app/src/components/CartCounter/CartCounter.jsx
@@ -1,10 +1,36 @@
 import './cartCounter.css';
+import useCartStore from '../../stores/useCartStore';
+import { useEffect, useState } from 'react';
 
 function CartCounter() {
+	const { cart } = useCartStore();
+	const [cartBalance, setCartBalance] = useState(null);
+
+	// När cart ändras så kommer cartCounter att synas/tas bort
+	useEffect(() => {
+		// Kontroll så att cart finns och inte är tom
+		if (cart && cart.length > 0) {
+			let cartTotalQty = 0;
+			// Loopa igenom cart för att lägga in det i cartQty
+			cart.forEach((event) => {
+				cartTotalQty += event.qty;
+			});
+
+			setCartBalance(cartTotalQty);
+		} else {
+			// Om cart är tom igen så kommer cartCounter försvinna
+			setCartBalance(null);
+		}
+	}, [cart]);
+
 	return (
-		<div className='cart-counter__box'>
-			<span className='cart-counter__number'>22</span>
-		</div>
+		cartBalance && (
+			<>
+				<div className='cart-counter__box'>
+					<span className='cart-counter__number'>{cartBalance}</span>
+				</div>
+			</>
+		)
 	);
 }
 
